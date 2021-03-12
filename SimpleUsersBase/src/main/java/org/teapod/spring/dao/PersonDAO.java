0301605e7d4e7338270b19c34fc0ab7cc27c8632
Teapod <37874495+Teapod@users.noreply.github.com>
2021-03-12 18:47:15 +0200

    Update PersonDAO.java

--- SimpleUsersBase/src/main/java/org/teapod/spring/dao/PersonDAO.java
+++ SimpleUsersBase/src/main/java/org/teapod/spring/dao/PersonDAO.java
@@ -20,28 +20,28 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM \"Person\"", new BeanPropertyRowMapper<>(Person.class));
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM \"Person\" WHERE id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
 
-        jdbcTemplate.update("INSERT INTO \"Person\" VALUES (?, 2, ?, ?)", person.getAge(), person.getName(),
+        jdbcTemplate.update("INSERT INTO Person VALUES (?, 2, ?, ?)", person.getAge(), person.getName(),
                 person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
 
-        jdbcTemplate.update("UPDATE \"Person\" SET age = ?, name = ?, email = ? WHERE id = ?", updatedPerson.getAge(), updatedPerson.getName(),
+        jdbcTemplate.update("UPDATE Person SET age = ?, name = ?, email = ? WHERE id = ?", updatedPerson.getAge(), updatedPerson.getName(),
                 updatedPerson.getEmail(), id );
 
     }
 
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM \"Person\" WHERE id = ?", id);
+        jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
     }
 }
